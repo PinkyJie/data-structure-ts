f0fec10fc0609606c29f1e4cc0abcd4ffde6bcf5
Wenbo Jie <pinkyjie.gn@gmail.com>
2021-06-26 23:46:58 +1000

    optimize Disjoint Set implementation
    * use parents[] and sizes[]

--- graph/disjoint-set.ts
+++ graph/disjoint-set.ts
@@ -3,56 +3,64 @@
  */
 export class DisjointSet {
   /**
-   * `roots` array: index is the vertex, value is their root vertex.
-   * e.g. if there is an edge [0,1], then vertex 0's root is vertex 1.
+   * `parents` array: index is the vertex, value is its parent vertex.
+   * e.g. if there is an edge [1,0] (1 -> 0) then vertex 0's parent is vertex 1.
    */
-  private roots: number[] = [];
+  private parents: number[] = [];
 
   /**
-   * `ranks` array: by default all vertices have rank 0, when we have a edge [0, 1],
-   * we should decide who is whose root by their ranks:
-   *    - if the two vertices have the same rank (e.g. for 1st edge, they are both 0),
-   * we do `roots[0] = 1`, then `ranks[1]++` (root vertex's rank gets increased)
-   *    - if the two vertices have different ranks, then the one with larger rank should
-   * be the root.
+   * `sizes` array: by default all vertices have size 1 (only has 1 vertex),
+   * when we have a edge [1, 0], we should decide who should be the new parent based on
+   * their existing parent vertex's size:
+   *    - if the two parents have the same sizes (e.g. for 1st edge, they are both 1),
+   * we do `parents[P0] = P1`, then `sizes[P1] += sizes[P0]`, cause we make P1 as P0's
+   * parent (P1 -> P0), we need to add P0's size on P1
+   *    - if the two parents have different sizes, then the one with larger size should
+   * be the parent.
    *
-   * This enhancement is called `path compression`, if we think the graph as a tree, this
-   * will reduce the height of the tree, which will accelerate the speed for `find()`.
+   * This enhancement is called `balancing optimization`, if we think the graph as a
+   * tree, this will reduce the height of the tree, which will accelerate the speed
+   * for `find()`.
    */
-  private ranks: number[] = [];
+  private sizes: number[] = [];
 
   constructor(numberOfVertices: number) {
     for (let i = 0; i < numberOfVertices; i++) {
-      // by default each vertex's root is itself
-      this.roots.push(i);
-      this.ranks.push(0);
+      // by default each vertex's parent is itself
+      this.parents.push(i);
+      this.sizes.push(1);
     }
   }
 
   /**
    * When there is an edge in the graph, these 2 vertices can be `union`: find the
-   * roots for these 2 vertices and update the `roots` array. O(1)
+   * parents for these 2 vertices and update the `parents` array. O(1)
    */
   union(source: number, destination: number): void {
-    const rootForSource = this.find(source);
-    const rootForDest = this.find(destination);
+    const parentForSource = this.find(source);
+    const parentForDest = this.find(destination);
 
-    if (this.ranks[rootForSource] > this.ranks[rootForDest]) {
-      this.roots[rootForDest] = rootForSource;
+    if (parentForSource === parentForDest) {
+      return;
+    }
+
+    if (this.sizes[parentForSource] > this.sizes[parentForDest]) {
+      this.parents[parentForDest] = parentForSource;
+      this.sizes[parentForSource] += this.sizes[parentForDest];
     } else {
-      this.roots[rootForSource] = rootForDest;
-      if (this.ranks[rootForSource] === this.ranks[rootForDest]) {
-        this.ranks[rootForDest]++;
-      }
+      this.parents[parentForSource] = parentForDest;
+      this.sizes[parentForDest] += this.sizes[parentForSource];
     }
   }
 
-  /** Find the root vertex of the passed in `vertex`: O(1) amortized */
+  /** Find the parent vertex of the passed in `vertex`: O(1) amortized */
   find(vertex: number): number {
-    if (this.roots[vertex] !== vertex) {
-      // keep updating vertex's root to its root's root, path compression
-      this.roots[vertex] = this.find(this.roots[vertex]);
+    while (this.parents[vertex] !== vertex) {
+      // keep updating vertex's parent to its parent's parent, path compression
+      this.parents[vertex] = this.parents[this.parents[vertex]];
+      vertex = this.parents[vertex];
     }
-    return this.roots[vertex];
+    // we can return `vertex` directly because it's equal to `parents[vertex]`
+    return vertex;
   }
 }
